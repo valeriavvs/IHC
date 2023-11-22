69568ac94f6ffdc32f977f1a9d1004ccdae69588
SainouP <131126337+SainouP@users.noreply.github.com>
2023-11-22 03:09:30 -0500

    Add files via upload

---
+++ TF_IHC/nuevo1as.js
@@ -0,0 +1,7 @@
+function mostrarMenu() {
+    document.getElementById("contenidoDesplegable").style.display = "block";
+}
+
+function ocultarMenu() {
+    document.getElementById("contenidoDesplegable").style.display = "none";
+}
